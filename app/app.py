2d982e7f4b3cc8c0719d07d2cee7f76be6951565
Anthony PERIQUET <anthony@periquet.net>
2020-08-31 14:54:45 +0200

    Feat : Compute stats

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from abc import ABC, abstractmethod
-from datetime import datetime
+from collections import namedtuple
+from datetime import datetime, timedelta
+from statistics import mean
 import argparse
+import copy
 import docker
 import json
 import logging
@@ -16,7 +19,7 @@ class DockerHelper(ABC):
     def __init__(self, client=docker.from_env(), logger=None):
         self.client = client
         self.logger = logging.getLogger("autoscaler") if logger is None else logger
-        self.states = []
+        self.states = {}
 
     @abstractmethod
     def monitor(self):
@@ -41,16 +44,88 @@ class ServiceHelper(DockerHelper):
         )
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
-        channel.basic_consume(queue='autoscaling.stats', on_message_callback=self.compute_stats, auto_ack=True)
+        channel.basic_consume(queue='autoscaling.stats', on_message_callback=self.register_stats, auto_ack=True)
         channel.start_consuming()
 
 
-    def compute_stats(self, channel, method, properties, body):
-        items = self.get_items()
-        # for item in items:
-        #     self.logger.debug(item.attrs)
-        #     ['Spec']['Mode']['Replicated']['Replicas']
-        self.logger.debug(f'{body} is received')
+    def register_stats(self, channel, method, properties, body):
+        metrics = json.loads(body,object_hook=lambda d: namedtuple('X', d.keys())(*d.values()));
+        self.states[metrics.service.name] = self.states[metrics.service.name] if metrics.service.name in self.states else {}
+        self.states[metrics.service.name] = self.mergeDict(self.states[metrics.service.name], {
+            "id": metrics.service.id,
+            "tasks": {
+                metrics.service.task: { "metrics": { metrics.timestamp: { "cpu": metrics.stats.cpu, "mem": metrics.stats.memory} } }
+            }
+        })
+
+        self.cleanMetrics()
+        self.cleanServices()
+        self.check_stats()
+
+    def check_stats(self):
+        ''' Extract metrics for each service's task '''
+        for service_name, content in self.states.items():
+            for task in content["tasks"].values():
+                cpu = []
+                mem = []
+                for metrics in task["metrics"].values():
+                    cpu.append(metrics["cpu"])
+                    mem.append(metrics["mem"])
+
+                if self.isOverloading(cpu, mem):
+                    self.scale_up(content["id"])
+                else:
+                    self.scale_down(content["id"])
+
+    def isOverloading(self, cpu, mem):
+        return mean(cpu) > 50 or mean(mem) > 50
+
+    def scale_up(self, service_id):
+        ''' Scale up if possible (mode = replicated) '''
+        service = self.client.services.get(service_id)
+        if self.is_service_replicated(service):
+            service.scale(self.get_service_replicas(service) + 1)
+            print(f'Scaling up service {self.get_service_name(service)}')
+
+    def scale_down(self, service_id):
+        ''' Scale down if possible (mode = replicated, scale > 1) '''
+        service = self.client.services.get(service_id)
+        if self.is_service_replicated(service) and self.get_service_replicas(service) > 1:
+            service.scale(self.get_service_replicas(service) - 1)
+            print(f'Scaling down service {self.get_service_name(service)}')
+
+    def mergeDict(self, dict1, dict2):
+        ''' Merge dictionaries and keep values of common keys in list'''
+        dict3 = {**dict1, **dict2}
+        for key, value in dict3.items():
+            if key in dict1 and key in dict2:
+                if isinstance(dict3[key], dict):
+                    dict3[key] = self.mergeDict(dict1[key], dict2[key])
+                elif dict1[key] != dict2[key]:
+                    dict3[key] = [value , dict2[key]]
+                else: 
+                    dict3[key] = value
+
+        return dict3
+
+    def cleanMetrics(self):
+        ''' Clean metrics older than 5 minutes '''
+        output = copy.deepcopy(self.states)
+        for service, content in self.states.items():
+            for name, task in content["tasks"].items():
+                for timestamp, metrics in task["metrics"].items():
+                    if datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S") < (datetime.now() - timedelta(minutes=5)):
+                        del(output[service]["tasks"][name]["metrics"][timestamp])
+
+        self.states = output
+
+    def cleanServices(self):
+        items = self.get_items();
+        services = []
+        for item in items:
+            services.append(item.name)
+        for service in [item for item in self.states.keys() if item not in services]:
+            del(self.states[service])
 
     def get_items(self):
         try:
@@ -61,6 +136,21 @@ class ServiceHelper(DockerHelper):
 
     def get_stats(self, item):
         return {}
+
+    def is_service_replicated(self, service):
+        is_replicated = False
+        try:
+            is_replicated = service.attrs['Spec']['Mode']['Replicated']
+        except:
+            pass
+
+        return is_replicated
+
+    def get_service_name(self, service):
+        return service.attrs['Spec']['Name']
+
+    def get_service_replicas(self, service):
+        return service.attrs['Spec']['Mode']['Replicated']['Replicas'];
 
 class NodeHelper(DockerHelper):
     def __init__(self, client, logger=None):
